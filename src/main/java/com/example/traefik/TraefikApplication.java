d0d8d8794b6f52eac60340814bd7e60499e164df
kimzini <rlawlsdl11111@gmail.com>
2025-02-21 21:13:03 +0900

    chore: 개발 환경 세팅

---
+++ src/main/java/com/example/traefik/TraefikApplication.java
@@ -0,0 +1,13 @@
+package com.example.traefik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraefikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraefikApplication.class, args);
+	}
+
+}
